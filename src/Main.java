ac3d56c255416f8306a08418ec050756d24dd1cc
Ablagochestivyy <tonchek123@gmail.com>
2024-04-22 18:32:02 +0300

    add massive with objects

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,30 @@
 public class Main {
     public static void main(String[] args) {
 
+        Griffindor[] grifindorStudents = {
+            new Griffindor("Гари", "Поттер", 100, 100, 100, 92, 4000),
+            new Griffindor("Гермиона", "Грейнджер", 85, 90, 90, 85, 2700),
+            new Griffindor("Рон", "Уизли", 100, 100, 100, 92, 1569),
+        };
+
+        Puffendui[] puffenduiStudesnts = {
+                new Puffendui("Захария", "Смит", 95, 78,85, 54, 1298),
+                new Puffendui("Седрик", "Диггори", 95, 78,85, 54, 1298),
+                new Puffendui("Джастин", "Финч-Флетчли", 75, 49,83, 74, 2145),
+        };
+
+        Kogtevran[] kogtevranStudents = {
+                new Kogtevran("Чжоу", "Чанг", 59, 83, 96, 92, 70, 3564),
+                new Kogtevran("Падма", "Патил", 86, 84, 21, 35, 85, 2967),
+                new Kogtevran("Маркус", "Белби", 98, 89,78,85, 90, 3864),
+        };
+
+        Slizerin[] slizerinStudents = {
+                new Slizerin("Драко", "Малфой", 89, 98, 88, 95, 100, 87, 3200),
+                new Slizerin("Грэхэм", "Монтегю", 65, 78, 81, 59, 75, 72, 2763),
+                new Slizerin("Гойл", "Грегори", 28, 94, 46, 75, 69, 57, 2489),
+        };
+
 
     }
 }
